d6297ac097721cc828ba337328e14ef5d187e005
Olle Kvarnström <olle+git@iix.se>
2017-05-03 21:21:39 +0200

    Get basic navbar up and running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http} from "@angular/http";
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    templateUrl: './app.component.html'
 })
-export class AppComponent {
-    title = 'app works!';
+export class AppComponent implements OnInit {
+    user: { username: string, password: string};
+    errorMessage: string;
+    loggedIn: boolean;
+
+    constructor(
+        private http: Http
+    ) {
+    }
+
+    ngOnInit(): void {
+        this.loggedIn = false;
+        this.errorMessage = undefined;
+        this.user = {
+            username: '',
+            password: ''
+        };
+    }
+
+    login() {
+        this.http.post('/api/login', this.user).subscribe(
+            res => this.loginSuccess(res),
+            err => this.loginError(err)
+        );
+    }
+
+    logout() {
+         this.loggedIn = false;
+         this.http.delete('/api/login').subscribe();
+    }
+
+    checkLoggedIn() {
+         this.http.get('/api/login').subscribe(
+             res => this.loginSuccess(res)
+         );
+    }
+
+    loginSuccess(data: any) {
+        console.log('loginSuccess', data)
+        this.loggedIn = true;
+        this.errorMessage = undefined;
+        this.user.password = '';
+    }
+
+    loginError(data: string) {
+        console.log('loginError', data)
+        this.loggedIn = false;
+        this.errorMessage = data;
+    }
 }
---
+++ src/app/main/page-not-found.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  template: `
+<div>
+  <h1>The page you were looking for doesn't exist.</h1>
+  <p>You may have mistyped the address or the page may have moved.</p>
+</div>`
+})
+export class PageNotFoundComponent { }
