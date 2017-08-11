fa5e81b79aaa0eb2f2b618b5a2f373dd0539f7c5
Olle Kvarnström <olle+git@iix.se>
2017-08-11 20:38:01 +0200

    lint

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -10,38 +10,37 @@ import {ModalService} from "../common/modal.service";
 @Component({
     selector: 'notes',
     template: `
-<h1>
-    Notes
-    <button [hidden]="!isLoggedIn" routerLink="/notes/new" class="btn btn-outline-success">
-        <span class="fa fa-plus"></span>
-        Add note
-    </button>
-</h1>
-<form>
-    <div class="form-group">
-        <div class="input-group">
-        <div class="input-group-addon"><span class="fa fa-search"></span></div>
-            <input type="text"
-                   class="form-control"
-                   placeholder="Filter"
-                   [formControl]="filterText$"
-                   autocomplete="off"
-                   name="filterText">
-        </div>
-    </div>
-</form>
-<loading-spinner [hidden]="!isLoadingData"></loading-spinner>
-<div [hidden]="isLoadingData" class="list-group">
-    <a *ngFor="let note of filteredNotes"
-       routerLink="/notes/{{ note.id }}"
-       class="list-group-item list-group-item-action justify-content-between">
-        &raquo;
-        {{ note.title }}
-        <h6 class="m-0">
-            <span class="badge badge-primary badge-pill" [innerText]="note.date"></span>
-        </h6>
-    </a>
-</div>`
+        <h1>
+            Notes
+            <button [hidden]="!isLoggedIn" routerLink="/notes/new" class="btn btn-outline-success">
+                <span class="fa fa-plus"></span>
+                Add note
+            </button>
+        </h1>
+        <form>
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-addon"><span class="fa fa-search"></span></div>
+                    <input class="form-control"
+                        placeholder="Filter"
+                        [formControl]="filterText$"
+                        autocomplete="off"
+                        name="filterText">
+                </div>
+            </div>
+        </form>
+        <loading-spinner [hidden]="!isLoadingData"></loading-spinner>
+        <div [hidden]="isLoadingData" class="list-group">
+            <a *ngFor="let note of filteredNotes"
+               routerLink="/notes/{{ note.id }}"
+               class="list-group-item list-group-item-action justify-content-between">
+                &raquo;
+                {{ note.title }}
+                <h6 class="m-0">
+                    <span class="badge badge-primary badge-pill" [innerText]="note.date"></span>
+                </h6>
+            </a>
+        </div>`
 })
 
 export class NotesComponent implements OnInit {
